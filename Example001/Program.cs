90ed43939aed0ef77003913bfcd46c3b0f55af06
ababoi4 <105204537+ababoi4@users.noreply.github.com>
2022-08-10 12:16:59 +0300

    Add Example001

---
+++ Example001/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string readVelue = Console.ReadLine();
+    int result = int.Parse(readVelue);
+    return result;
+}
+
+int userNum = Prompt("Введите трехзначное число ->");
+
+int result = (userNum % 100) / 10;
+
+Console.WriteLine(result);
