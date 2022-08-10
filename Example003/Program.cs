71f6c49f5d29dee49c72a7a88ba0b82aba36261a
ababoi4 <105204537+ababoi4@users.noreply.github.com>
2022-08-10 13:57:00 +0300

    Add Example003

---
+++ Example003/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 3: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string readVelue = Console.ReadLine();
+    int result = int.Parse(readVelue);
+    return result;
+}
+
+int userNum = Prompt("Введите число ->");
+int userNum1 = userNum % 10;
+if(userNum > 99) 
+{
+    System.Console.WriteLine($"{userNum1}");
+}
+else 
+{
+    System.Console.WriteLine($"Третьей цыфры нет");
+}
